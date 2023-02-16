6271e24602b4ea60d493f221348911cb3401bcbf
Guillermo514 <125508192+Guillermo514@users.noreply.github.com>
2023-02-16 11:22:18 -0600

    Remove unnecesary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -16,8 +16,6 @@
 //     })
 // }
 
-console.log('Archivo de JavaScript Cargado')
-
 // fetch('https://puzzle.mead.io/puzzle').then((response) => {
 //     response.json().then((data) => {
 //         console.log(data)
